a0b61880898e45df3991b6e1860ac800418f317e
frankzhang <frank.zhang.sa.au@gmail.com>
2024-05-19 14:20:12 +1000

    1. demo: how to use styled components to define Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 import "./App.css";
+import { StyledButton } from "./components/StyledButton";
 
 function App() {
-  return <div className="App">Hello world!</div>;
+  return (
+    <div className="App">
+      <StyledButton>My self defined button</StyledButton>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/StyledButton.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+export const StyledButton = styled.button`
+  background-color: red;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 4px;
+
+  &:hover {
+    background-color: #45a049;
+  }
+`;
