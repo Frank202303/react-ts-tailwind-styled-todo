69f7454417798ddfcd9e9c368039633ff2169147
frankzhang <frank.zhang.sa.au@gmail.com>
2024-05-20 15:11:57 +1000

    1. how to use 'useRef'

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Button, Input, InputWrapper } from "../style";
 
 interface Props {
@@ -8,13 +8,21 @@ interface Props {
 }
 
 const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
-    <InputWrapper action="" onSubmit={handleAdd}>
+    <InputWrapper
+      action=""
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
       <Input
         type="input"
         placeholder="Enter a Task"
         value={todo}
         onChange={(e) => setTodo(e.target.value)}
+        ref={inputRef}
       />
       <Button type="submit">Go</Button>
     </InputWrapper>
